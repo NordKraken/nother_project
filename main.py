0d3bfa1c7c20049b68fa28bc797b1fac8e3446c4
FabioXXI <astore.a.fabio@gmail.com>
2024-04-23 15:53:49 -0300

    cool

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import json
 from logic_interpreter.logic import Logic
-from logic_interpreter.type import Type
-from logic_interpreter.stack import Stack
 from logic_interpreter.functions.validator import identify
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
 try:
-    identify("f", "=", "f")
+    test = Logic(data)
+    test.ifObj.interpreter()
 except:
     err = Exception("A error occurs")
     print(err)
