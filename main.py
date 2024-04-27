a143394c7f702bcb8e1336e0cebeea639a8a49ca
FabioXXI <astore.a.fabio@gmail.com>
2024-04-27 10:19:31 -0300

    stable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
 from logic_interpreter.logic import Logic
-from logic_interpreter.functions.validator import identify
-from logic_interpreter.data_structures.stack import Stack
-from logic_interpreter.data_structures.queue import Queue
 
 with open('data.json', 'r') as file:
     data = json.load(file)
